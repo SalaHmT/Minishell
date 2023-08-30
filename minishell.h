30fb6a85b608f683c84af1c6c9492ecda86a04c1
YuSuFLams <yousseflamssieh@gmail.com>
2023-08-30 16:17:39 +0100

    ok

---
+++ minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylamsiah <ylamsiah@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 14:27:18 by ylamsiah          #+#    #+#             */
+/*   Updated: 2023/08/30 14:46:00 by ylamsiah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <signal.h>
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <unistd.h>
+# include <term.h>
+# include <curses.h>
+
+#endif 
