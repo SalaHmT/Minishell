9be70246d424ed66545ec6799726f17b4b91ce9c
YuSuFLams <yousseflamssieh@gmail.com>
2023-09-07 21:54:13 +0100

    ylmasiah

--- src/parsing/signal_mshell.c
+++ src/parsing/signal_mshell.c
@@ -6,7 +6,7 @@
 /*   By: ylamsiah <ylamsiah@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 22:24:48 by ylamsiah          #+#    #+#             */
-/*   Updated: 2023/09/07 17:38:29 by ylamsiah         ###   ########.fr       */
+/*   Updated: 2023/09/07 21:41:03 by ylamsiah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void sig_ctlc(int input_sig)
     if (input_sig == SIGINT)
     {
 		ft_putchar_fd('\n', STDERR_FILENO);
-		// rl_replace_line("", STDIN_FILENO);
+		rl_replace_line("", STDIN_FILENO);
 		rl_on_new_line();
 		rl_redisplay();
 	}
