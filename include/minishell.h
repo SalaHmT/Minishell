f28a4a4ab999223cb83d48e86bee0a1d26285571
Salah Hamsate <shamsate@e1r3p13.1337.ma>
2023-12-09 19:17:18 +0100

    add some fct... for lexer   :)

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 17:28:44 by shamsate          #+#    #+#             */
-/*   Updated: 2023/12/08 23:22:36 by shamsate         ###   ########.fr       */
+/*   Updated: 2023/12/09 18:47:56 by shamsate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,6 +110,8 @@ void		command_init(t_comd	*cmd);
 int			is_reder(int var);
 int			check_reder_tkn(t_tkn **data);
 int			check_pipe_red_herdoc(t_tkn **data);
+int			is_a_directory(char *str);
+int			check_cmd_isdretory(char *str);
 //lexer
 int			is_red_char(char c);
 void		add_operator_tkn(t_tkn **data, char *cmd, int *idx);
--- src/parsing/tkn_data_check_2.c
+++ src/parsing/tkn_data_check_2.c
@@ -6,8 +6,50 @@
 /*   By: shamsate <shamsate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 23:21:50 by shamsate          #+#    #+#             */
-/*   Updated: 2023/12/08 23:23:37 by shamsate         ###   ########.fr       */
+/*   Updated: 2023/12/09 18:47:18 by shamsate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+int	is_a_directory(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '/')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_cmd_isdretory(char *str)
+{
+	DIR		*dir;
+
+	if (str && is_a_directory(str))
+	{
+		if (!str)
+		{
+			(ft_putstr("bashn't: ", 2), ft_putstr(str, 2));
+			ft_putstr(" :is a directory\n", 2);
+			return (g_lb_data.ext_status = CMD_EXE_FAILED, 1);
+		}
+		else
+		{
+			dir = opendir(str);
+			if (dir)
+			{
+				(ft_putstr("bashn't: ", 2), ft_putstr(str, 2));
+				ft_putstr(" :is a directory\n", 2);
+				if (dir)
+					closedir(dir);
+				return (g_lb_data.ext_status = CMD_EXE_FAILED, 1);
+			}
+		}
+	}
+	return (0);
+}
